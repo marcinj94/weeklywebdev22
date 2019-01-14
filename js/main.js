4a39b68eeb47145d1017febb4af45b4d83018147
beardofdoom <marcin.jusiak@gmail.com>
2019-01-14 22:23:09 +0100

    Main navbar and minibar section

---
+++ js/main.js
@@ -0,0 +1,25 @@
+btnItems = document.querySelectorAll('.hamburger__item');
+navItems = document.querySelectorAll('.nav__item');
+
+nav = document.querySelector('.main-navbar__nav');
+navList = document.querySelector('.nav__list');
+btn = document.querySelector('.main-navbar__hamburger');
+
+btn.addEventListener('click', function () {
+    btnItems.forEach(navItem => {
+        navItem.classList.toggle('active');
+    });
+    nav.classList.toggle('active');
+    navList.classList.toggle('active');
+
+});
+
+navItems.forEach(navItem => {
+    navItem.addEventListener('click', () => {
+        btnItems.forEach(btnItem => {
+            btnItem.classList.remove('active');
+        });
+        nav.classList.remove('active');
+        navList.classList.remove('active');
+    });
+})
