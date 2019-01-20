b24b4f7bc37881a3f42ba9ae8f0f6ae0e3bf408b
beardofdoom <marcin.jusiak@gmail.com>
2019-01-20 22:49:20 +0100

    Add header scroll effect
    	-Added header scroll effect (brightness) [jQuery]
    	- Fixed problem with navbar(mobile) and video section title
    	- Deleted console.log's
    	- Deleted commented sections

--- js/main.js
+++ js/main.js
@@ -1,135 +1,159 @@
 // Menu
-btnItems = document.querySelectorAll('.hamburger__item');
-navItems = document.querySelectorAll('.nav__item');
-nav = document.querySelector('.main-navbar__nav');
-navList = document.querySelector('.nav__list');
-btn = document.querySelector('.main-navbar__hamburger');
+btnItems = document.querySelectorAll(".hamburger__item");
+navItems = document.querySelectorAll(".nav__item");
+nav = document.querySelector(".main-navbar__nav");
+navList = document.querySelector(".nav__list");
+btn = document.querySelector(".main-navbar__hamburger");
 
-btn.addEventListener('click', function () {
-    btnItems.forEach(navItem => {
-        navItem.classList.toggle('active');
-    });
-    nav.classList.toggle('active');
-    navList.classList.toggle('active');
-    btn.classList.toggle('active');
-
+btn.addEventListener("click", function() {
+  btnItems.forEach(navItem => {
+    navItem.classList.toggle("active");
+  });
+  nav.classList.toggle("active");
+  navList.classList.toggle("active");
+  btn.classList.toggle("active");
 });
 
 navItems.forEach(navItem => {
-    navItem.addEventListener('click', () => {
-        btnItems.forEach(btnItem => {
-            btnItem.classList.remove('active');
-        });
-        nav.classList.remove('active');
-        navList.classList.remove('active');
-        btn.classList.remove('active');
+  navItem.addEventListener("click", () => {
+    btnItems.forEach(btnItem => {
+      btnItem.classList.remove("active");
     });
+    nav.classList.remove("active");
+    navList.classList.remove("active");
+    btn.classList.remove("active");
+  });
 });
-
 
 //Portfolio
 
-portfolioBtns = document.querySelectorAll('.portfolio__btn');
-portfolioProjects = document.querySelectorAll('.portfolio__project')
+portfolioBtns = document.querySelectorAll(".portfolio__btn");
+portfolioProjects = document.querySelectorAll(".portfolio__project");
 
 function userChoice() {
-    portfolioBtns.forEach(portfolioBtn => {
-        portfolioBtn.classList.remove('active');
-    });
+  portfolioBtns.forEach(portfolioBtn => {
+    portfolioBtn.classList.remove("active");
+  });
 
-    this.classList.add('active');
+  this.classList.add("active");
 
-    portfolioProjects.forEach(portfolioProject => {
-        portfolioProject.classList.remove('disable');
-        if (this.dataset.group) {
-            if (portfolioProject.dataset.group != this.dataset.group) {
-                portfolioProject.classList.add('disable');
-            }
-        }
-    });
-};
+  portfolioProjects.forEach(portfolioProject => {
+    portfolioProject.classList.remove("disable");
+    if (this.dataset.group) {
+      if (portfolioProject.dataset.group != this.dataset.group) {
+        portfolioProject.classList.add("disable");
+      }
+    }
+  });
+}
 
 portfolioBtns.forEach(portfolioBtn => {
-    portfolioBtn.addEventListener('click', userChoice);
+  portfolioBtn.addEventListener("click", userChoice);
 });
 
 // Fact section
-const factSection = document.querySelector('.fact');
-const factList = document.querySelector('.fact__list');
+const factSection = document.querySelector(".fact");
+const factList = document.querySelector(".fact__list");
 const factSectionFromTop = factSection.offsetTop;
 
-window.addEventListener('scroll', function () {
-    let scrollValue = window.scrollY;
-    if (scrollValue - 50 > factSectionFromTop) {
-        factList.classList.add('active');
-    }
+window.addEventListener("scroll", function() {
+  let scrollValue = window.scrollY;
+  if (scrollValue - 50 > factSectionFromTop) {
+    factList.classList.add("active");
+  }
 });
-
 
 //Clients section
-clientImages = document.querySelectorAll('.client__photo');
-clientTitle = document.querySelector('.client__title');
-clientDescription = document.querySelector('.client__description');
-clientQuotes = document.querySelectorAll('.client__quote');
+clientImages = document.querySelectorAll(".client__photo");
+clientTitle = document.querySelector(".client__title");
+clientDescription = document.querySelector(".client__description");
+clientQuotes = document.querySelectorAll(".client__quote");
+
+function addTextContent(title, description) {
+  clientTitle.textContent = title;
+  clientDescription.textContent = description;
+}
 
 function clientChoice() {
-    clientImages.forEach(clientImage => {
-        clientImage.classList.remove('active');
-    });
-    clientQuotes.forEach(clientQuote => {
-        clientQuote.classList.remove('active');
-        if (clientQuote.dataset.client == this.dataset.client) {
-            clientQuote.classList.add('active');
-        }
-    });
-
-    this.classList.add('active');
-    if (this.dataset.client == '1') {
-        clientTitle.textContent = 'Anthony Green';
-        clientDescription.textContent = 'Web developer';
-    } else if (this.dataset.client == '2') {
-        clientTitle.textContent = 'Kyle Roberts';
-        clientDescription.textContent = 'Company CEO';
-    } else if (this.dataset.client == '3') {
-        clientTitle.textContent = 'Bella Morgan';
-        clientDescription.textContent = 'Project manager';
+  clientImages.forEach(clientImage => {
+    clientImage.classList.remove("active");
+  });
+  clientQuotes.forEach(clientQuote => {
+    clientQuote.classList.remove("active");
+    if (clientQuote.dataset.client == this.dataset.client) {
+      clientQuote.classList.add("active");
     }
+  });
+
+  this.classList.add("active");
+  if (this.dataset.client == "1") {
+    addTextContent("Anthony Green", "Web developer");
+  } else if (this.dataset.client == "2") {
+    addTextContent("Kyle Roberts", "Company CEO");
+  } else if (this.dataset.client == "3") {
+    addTextContent("Bella Morgan", "Project manager");
+  }
 }
 clientImages.forEach(clientImage => {
-    clientImage.addEventListener('click', clientChoice);
+  clientImage.addEventListener("click", clientChoice);
 });
-
 
 // jQuery
 
-
 //smooth scroll nav items
-$('.nav__link').on('click', function () {
-    const menuItem = $(this).attr('href');
-    $('body, html').animate({
-        scrollTop: $(menuItem).offset().top
-    }, 3000);
+$(".nav__link").on("click", function() {
+  const menuItem = $(this).attr("href");
+  $("body, html").animate(
+    {
+      scrollTop: $(menuItem).offset().top
+    },
+    3000
+  );
 });
 
 // show|hide .up-btn
-const $upBtn = $('.up-btn');
+const $upBtn = $(".up-btn");
 
-$(window).on('scroll', function () {
-    const scrollValue = $(window).scrollTop();
-    return (scrollValue > 0) ? $upBtn.addClass('active') : $upBtn.removeClass('active')
-})
+$(window).on("scroll", function() {
+  const scrollValue = $(window).scrollTop();
+  return scrollValue > 0
+    ? $upBtn.addClass("active")
+    : $upBtn.removeClass("active");
+});
 
 //smooth scroll .up-btn to top
-$upBtn.on('click', function () {
-    $('body, html').animate({
-        scrollTop: $('body').offset().top
-    }, 2000);
+$upBtn.on("click", function() {
+  $("body, html").animate(
+    {
+      scrollTop: $("body").offset().top
+    },
+    2000
+  );
 });
 
 //smooth scroll in header hto next section
-$('.header__scroll').on('click', function () {
-    $('body, html').animate({
-        scrollTop: $('.services').offset().top
-    }, 1000);
+$(".header__scroll").on("click", function() {
+  $("body, html").animate(
+    {
+      scrollTop: $(".services").offset().top
+    },
+    1000
+  );
 });
 
+//header scroll effect
+const $doc = $(document);
+const $header = $(".header");
+const $services = $(".services");
+
+$doc.on("scroll", function() {
+  const $scrollValue = $doc.scrollTop();
+  const $servicesOffset = $services.offset().top;
+  const $headerHeight = $header.outerHeight();
+
+  if ($scrollValue < $servicesOffset) {
+    $header.css({
+      filter: `brightness(${1 - $scrollValue / $headerHeight})`
+    });
+  }
+});
